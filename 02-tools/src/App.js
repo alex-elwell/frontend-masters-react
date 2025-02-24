294dc7082f86520dddb720889907c570cdf1ede9
Alex Elwell <alexander.hayden.elwell@gmail.com>
2025-02-24 18:37:14 +0000

    uploading workshop 2 + adding vite to workshop 1

--- 02-tools/src/App.js
+++ 02-tools/src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import { createRoot } from "react-dom/client";
 
 const Pizza = (props) => {
   return React.createElement("div", {}, [
@@ -27,5 +27,5 @@ const App = () => {
 };
 
 const container = document.getElementById("root");
-const root = ReactDOM.createRoot(container);
+const root = createRoot(container);
 root.render(React.createElement(App));
